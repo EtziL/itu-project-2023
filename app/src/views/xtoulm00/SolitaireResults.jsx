1528780a12fa1f0d7cf858207fc847188848ae4e
Matej Toul <mates097@seznam.cz>
2023-12-17 21:14:10 +0100

    Add SolitaireResults view with author information

--- app/src/views/xtoulm00/SolitaireResults.jsx
+++ app/src/views/xtoulm00/SolitaireResults.jsx
@@ -1,3 +1,9 @@
+/**
+ * Solitare results view.
+*
+* Author: Toul Matěj (xtoulm00)
+*/
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
